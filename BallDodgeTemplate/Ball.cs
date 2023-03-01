80857d9ac90b948f9bf60224c232253bd72a985e
Aimen Ansari <umeaansa555@ed.amdsb.ca>
2023-03-01 10:00:56 -0500

    lesson complete

---
+++ BallDodgeTemplate/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallDodgeTemplate
+{
+    internal class Ball
+    {
+        public int x, y, xSpeed, ySpeed;
+        public int size = 20;
+
+        public Ball(int _x, int _y, int _xSpeed, int _ySpeed)
+        {
+            this.x = _x;
+            this.y = _y;
+            this.xSpeed = _xSpeed;
+            this.ySpeed = _ySpeed;
+        }
+
+        public void Move( int screenWidth, int screenHeight)
+        {
+            xSpeed = 10;
+            ySpeed = 10;
+            x += xSpeed;
+            y += ySpeed;
+
+            if (x > screenWidth - size || x < 0)
+            {
+                xSpeed *= -1;
+            }
+            if (y > screenHeight - size || y < 0)
+            {
+                ySpeed *= -1;
+            }
+        }
+    }
+}
---
+++ BallDodgeTemplate/GameScreen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallDodgeTemplate
+{
+    public partial class GameScreen : UserControl
+    {
+
+        Ball chaseBall = new Ball(40, 100, 10,10);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+
+        public GameScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void engine_Tick(object sender, EventArgs e)
+        {
+
+            /*chaseBall.x += chaseBall.xSpeed;
+            chaseBall.y += chaseBall.ySpeed;*/
+
+            chaseBall.Move(this.Width, this.Height);
+
+            /*if(chaseBall.x > this.Width - chaseBall.size || chaseBall.x < 0)
+            {
+                chaseBall.xSpeed *= -1;
+            }
+            if (chaseBall.y > this.Height - chaseBall.size || chaseBall.y < 0)
+            {
+                chaseBall.ySpeed *= -1;
+            }*/
+
+
+            Refresh();
+        }
+
+        private void GameScreen_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(greenBrush, chaseBall.x, chaseBall.y, chaseBall.size, chaseBall.size);
+        }
+
+        private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+
+        private void GameScreen_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
